f5e00747b483c4394b815bb8319fecf43869b316
Riazul Karim Ivan <ivancse58@gmail.com>
2026-02-22 23:12:40 +0600

    Lib Build 2.4.0

--- ui_library/build.gradle.kts
+++ ui_library/build.gradle.kts
@@ -102,7 +102,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.tnm.android.core"
                 artifactId = "ui-library"
-                version = "2.3.8"
+                version = "2.4.0"
                 from(components["release"])
             }
         }
